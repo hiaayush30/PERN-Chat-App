3bbe693f7d03c84cfd22f594b1dbab87f8bc07b5
aayush <hiaayush30@gmail.com>
2025-02-26 22:21:47 +0530

    commit

--- burakorkmez pern-chat-app master frontend/src/types/global.d.ts
+++
@@ -1,7 +0,0 @@
-// global.d.ts is a special file. Types added here can be used globally in the project without importing them.
-
-type ConversationType = {
-	id: string;
-	fullName: string;
-	profilePic: string;
-};
