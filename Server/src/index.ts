6a002dc5b487ad117cb64d58501bcb726d92300d
aayush <hiaayush30@gmail.com>
2025-02-27 19:56:18 +0530

    changes for deployment

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -1,17 +1,19 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import cors from 'cors';
+import express from 'express';
 import authRouter from './Routes/auth.route';
 import messageRouter from './Routes/message.route';
 import cookieParser from 'cookie-parser';
-import {app, server} from './socket/socket'
+import { app, server } from './socket/socket'
+import path from 'path';
 
-app.use(cors({
-    origin: process.env.FE_DOMAIN,
-    credentials: true
-    // allows cookies, authentication headers, and TLS client certificates to be sent between the
-    // frontend and backend in cross-origin requests.
-}))
+// app.use(cors({
+//     origin: process.env.FE_DOMAIN,
+//     credentials: true
+//     // allows cookies, authentication headers, and TLS client certificates to be sent between the
+//     // frontend and backend in cross-origin requests.
+// }))
 app.use(cookieParser());
 
 app.get('/health', (req, res) => {
@@ -19,6 +21,18 @@ app.get('/health', (req, res) => {
 })
 app.use("/api/auth", authRouter);
 app.use("/api/message", messageRouter);
+
+//deployment
+if (process.env.NODE_ENV !== "development") {
+    app.use(express.static(path.join(__dirname,"/Frontend/dist")));
+    app.get("*",(req,res)=>{
+        res.sendFile(
+            path.join(__dirname,"Frontend","dist","index.html")
+        )
+    })
+    console.log = () => { }; // Disable console logs in production
+    console.error = console.error; // Keep error logs
+}
 
 server.listen(process.env.PORT, () => {
     console.log('server running on port ' + process.env.PORT);
